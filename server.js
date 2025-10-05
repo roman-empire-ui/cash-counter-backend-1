42a852c8004270c301c302d20bb301a0be3f3adc
Srinivas <122779862+roman-empire-ui@users.noreply.github.com>
2025-10-05 10:48:54 +0530

    changed server

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.json())
 
 
 const corsOptions = {
-    origin: ['https://cash-counter-frontend.vercel.app', 'http://localhost:5173'],
+  origin: [
+    /^https:\/\/cash-counter-frontend.*\.vercel\.app$/, // Regex to allow any preview URL
+    "http://localhost:5173"
+  ],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
